657049d79003299a116481906a2ee75893a088da
Christopher Batts <christopher.batts@orasi.com>
2018-02-14 14:36:19 -0500

    Exercise10_1 started

--- src/test/java/exercises/Exercise10_1.java
+++ src/test/java/exercises/Exercise10_1.java
@@ -1,5 +1,19 @@
 package exercises;
 
-public class Exercise10_1 {
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
+public class Exercise10_1 {
+	@Test
+	public void testExercise10_1() {
+		WebDriver driver = new ChromeDriver();
+		
+		driver.get("https://the-internet.herokuapp.com/");
+		
+		driver.findElement(By.linkText("Dynamic Loading")).click();
+		
+		
+	}
 }
